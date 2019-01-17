fba7501388269d595487e60e836e86859ef02bda
Amber Lynn <griffiam@gmail.com>
2019-01-17 12:58:11 -0800

    trying to add a commit

--- src/App.js
+++ src/App.js
@@ -191,6 +191,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        
         <main>
           <div className="nav-item1">
             <Link to="/adies" className="adie">Adie Library</Link>
